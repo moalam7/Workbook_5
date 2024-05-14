38019930f6bfbbe87c42ce16bafd32ee2cc35712
moalam7 <mohammedalamx@gmail.com>
2024-05-13 22:06:54 -0400

    init

---
+++ demos/demos.js
@@ -0,0 +1,22 @@
+"use strict";
+
+let course = {
+  id: 1,
+  name: "Intro to JavaScript",
+  classroom: "Pearson Hall Room 134",
+}; //object
+
+//course.name
+
+let courses = [
+  //array
+  {}, //object- course
+  {},
+]; //array
+
+let courseNames = [
+  //array
+  "Intro to JavaScript", //string
+  "Intro to CSS",
+  "Intro to HTML",
+]; //array
