8c945adacb03422ac097f27118392fdf5a0f0952
moalam7 <mohammedalamx@gmail.com>
2024-05-15 18:01:25 -0400

    index

---
+++ exercises/dynamic_images/scripts/index.js
@@ -0,0 +1,66 @@
+"use strict";
+//data
+// variable imageFiles gets declared and assigned an array []
+// which contains objects representing an image file
+let imageFiles = [
+  {
+    id: "1",
+    path: "https://placehold.co/300x300",
+    description: "placeholder 1",
+  },
+  {
+    id: "2",
+    path: "https://placehold.co/300x300",
+    description: "placeholder 2",
+  },
+  {
+    id: "3",
+    path: "https://placehold.co/300x300",
+    description: "placeholder 3",
+  },
+  {
+    id: "4",
+    path: "https://placehold.co/300x300",
+    description: "placeholder 4",
+  },
+  {
+    id: "5",
+    path: "https://placehold.co/300x300",
+    description: "placeholder 5",
+  },
+  {
+    id: "6",
+    path: "https://placehold.co/300x300",
+    description: "placeholder 6",
+  },
+  {
+    id: "7",
+    path: "https://placehold.co/300x300",
+    description: "placeholder 7",
+  },
+  {
+    id: "8",
+    path: "https://placehold.co/300x300",
+    description: "placeholder 8",
+  },
+  {
+    id: "9",
+    path: "https://placehold.co/300x300",
+    description: "placeholder 9",
+  },
+  {
+    id: "10",
+    path: "https://placehold.co/300x300",
+    description: "placeholder 10",
+  },
+];
+
+
+window.onload = function(){
+  //get all html elements into variables
+
+  //define  your functions
+
+
+  //associate your functions with html element events
+}
