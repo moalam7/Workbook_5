b8b0b09a3d3952faef0d49aa8ae9b9782d9b369c
moalam7 <mohammedalamx@gmail.com>
2024-05-15 09:15:04 -0400

    updates

--- demos/arrow_function.js
+++ demos/arrow_function.js
@@ -1,12 +1,45 @@
 "use strict";
 
-//before
-function multiply(a, b) {
+function mutliply(a, b) {
   return a * b;
+} //function declaration
+(a, b) => a * b; //arrow function
+
+function add(a, b) {
+  return a + b;
 }
+(a, b) => a + b;
 
-console.log(multiply(5, 5));
 
-//after
-let multiply2 = (a, b) => a * b;
-console.log(multiply2(5, 5));
+function divide(a,b) {
+    return a/b;
+}
+(a,b)=> a/b;
+
+function print(value){
+    console.log(value);
+}
+//value=> console.log(value);
+let print = value=> console.log(value);
+
+print("test")
+
+// function addThenDivideTheResult(a,b) {
+//     let result1 = add(a, b);
+//     let result2 = divide(result1, 1);
+//     return result2;
+// }
+
+// function add(a, b) {
+//   return a + b;
+// }
+
+// let result = add(2, 2);
+// console.log(result);
+
+// let add1 = (a, b) => {
+//   return a + b;
+// };
+
+// let result1 = add1(2, 2);
+// console.log(result1);
