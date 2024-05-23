079427010ec8d335c81d3ba9162c5dd58ae07b0a
moalam7 <mohammedalamx@gmail.com>
2024-05-23 11:50:18 -0400

    Section 3-1 finished

---
+++ workbook_notes/mod2/browser.js
@@ -0,0 +1,1 @@
+window.open("https://www.w3schools.com/js/js_htmldom.asp");
---
+++ workbook_notes/mod2/module2.js
@@ -0,0 +1,7 @@
+"use strict";
+
+//in JS window object represents the browser window and all its features
+
+//DOM - Document Object Model allows JS to interact with the page contents
+
+//BOM - Browser Object Model allows JS to interact with the browser
